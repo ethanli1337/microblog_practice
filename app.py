08e4bf937fe4c7cb274b6b1161a5e4b5b2323c6d
Ethan Li <ethanli2756@gmail.com>
2025-07-26 12:46:30 -0500

    first

---
+++ app.py
@@ -0,0 +1,34 @@
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from dotenv import load_dotenv 
+import os 
+
+
+
+app = Flask(__name__)
+db_url=os.getenv("DATABASE_URL")
+client=MongoClient(db_url)
+app.db=client.microblog
+
+entries=[]
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    print([e for e in app.db.entries.find({})])
+    if request.method=="POST":
+        entry_content=request.form.get("content")
+        formatted_date=datetime.datetime.today().strftime("%Y-%m-%d")
+        entries.append((entry_content, formatted_date))
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+    entries_with_date=[
+        (entry[0], entry[1], datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d"))
+        for entry in entries
+    ]
+
+    return render_template("home.html", entries=entries_with_date)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
