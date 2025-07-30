f2a98fb7dcfc8066d499ef7df9df9520c088e368
Ethan Li <ethanli2756@gmail.com>
2025-07-29 22:35:33 -0500

    jinja2 macros

--- app.py
+++ app.py
@@ -12,30 +12,38 @@ def create_app():
     client=MongoClient(db_url)
     app.db=client.microblog
 
-    entries=[]
 
-    @app.route("/", methods=['GET', 'POST'])
-    def home():
-        print([e for e in app.db.entries.find({})])
-        if request.method=="POST":
-            entry_content=request.form.get("content")
-            formatted_date=datetime.datetime.today().strftime("%Y-%m-%d")
-            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+    todos = [("Get milk", False), 
+            ("Learn programing", True)
+             ]
+    # entries=[]
 
-        entries_with_date=[
-            (
-                entry["content"], 
-                entry["date"], 
-                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+    # @app.route("/", methods=['GET', 'POST'])
+    # def home():
+    #     print([e for e in app.db.entries.find({})])
+    #     if request.method=="POST":
+    #         entry_content=request.form.get("content")
+    #         formatted_date=datetime.datetime.today().strftime("%Y-%m-%d")
+    #         app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+    #     entries_with_date=[
+    #         (
+    #             entry["content"], 
+    #             entry["date"], 
+    #             datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
             
-            )
-            for entry in app.db.entries.find({})
-        ]
+    #         )
+    #         for entry in app.db.entries.find({})
+    #     ]
 
-        return render_template("home.html", entries=entries_with_date)
+    #     return render_template("home.html", entries=entries_with_date)
     
 
-    @app.route("/jinja", methods=['GET', 'POST'])
+    # @app.route("/jinja", methods=['GET', 'POST'])
+    # def todo():
+    #     return render_template("home2.html", todos=["Get milk", "Learn programming"])
+
+    @app.route("/")
     def todo():
-        return render_template("home2.html", todos=["Get milk", "Learn programming"])
+        return render_template("home2.html", todos=todos)
     return app
